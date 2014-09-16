090607b2f8e2187d120a452959044a20b1c565f5
hawken <hawken@thehawken.org>
2014-09-16 19:25:05 +0200

    more unwrapping

--- js-bootstrap.js
+++ js-bootstrap.js
@@ -76,113 +76,112 @@
 		return r.module;
 	}(window)
 }(window);
-!function(n,r){
+!function(){
+	r = function(){
+		function n(n,r){
+			for(var e=0,t=n.length;t>e;++e){
+				if(!r(n[e])) return c;
+			}
+			return 1;
+		}
+		function r(r,e){
+			n(r,function(n){return!e(n)})
+		}
+		function e(i,u,c){
+			function f(n){
+				return n.call?n():d[n]
+			}
+			function s(){
+				if(!--$){
+					d[h]=1,v&&v();
+					for(var e in m){
+						n(e.split("|"),f)&&!r(m[e],f)&&(m[e]=[])
+					}
+				}
+			}
+			i=i[a]?i:[i];
+			var g=u && u.call,
+			    v=g ? u : c,
+			    h=g?i.join(""):u,
+			    $=i.length;
+			setTimeout(function(){
+				r(i,function n(r,e){
+					if(r === null){
+						return s();
+					}
+					r = e || /^https?:\/\//.test(r) || !o ? r : (o+r+".js");
+					if(p[r]){
+						h && l[h] = 1;
+						if(p[r] == 2){
+							return s();
+						} else {
+							setTimeout(n.bind(null,r,!0),0);
+						}
+						return void 0;
+					} else {
+						p[r] = 1;
+						h && (l[h] = 1);
+						t(r,s);
+						return void 0;
+					}
+				})
+			},0);
+			return e;
+		}
+		function t(n,r){
+			var e,
+			    t=i.createElement("script");
+			t.onload=t.onerror=t[s]=function(){
+				t[f] && !/^c|loade/.test(t[f]) || e || (t.onload=t[s]=null,e=1,p[n]=2,r());
+			};
+			t.async=1;
+			t.src=n;
+			u.insertBefore(t,u.lastChild);
+		}
+		var o,
+		    i=document,
+		    u=i.getElementsByTagName("head")[0],
+		    c=!1,
+		    a="push",
+		    f="readyState",
+		    s="onreadystatechange",
+		    d={},
+		    l={},
+		    m={},
+		    p={};
+		e.get = t;
+		e.order=function(n,r,t){
+			!function o(i){
+				i=n.shift(),n.length?e(i,o):e(i,r,t)
+			}()
+		};
+		e.path=function(n){
+			o=n;
+		};
+		e.ready=function(t,o,i){
+			t= t[a] ? t : [t];
+			var u=[];
+			!r(t,function(n){
+				d[n]||u[a](n)
+			}) && n(t,function(n){return d[n]}) ? o() : !function(n){
+				m[n]=m[n] || [];
+				m[n][a](o);
+				i&&i(u);
+			}(t.join("|"));
+			return e;
+		};
+		e.done=function(n){
+			e([null],n)
+		};
+		return e;
+	};
 	if(typeof module != "undefined" && module.exports){
 		module.exports = r()
 	} else if(typeof define == "function" && define.amd){
 		define(r)
 	} else {
-		this[n] = r()
+		this.$script = r()
 	}
-}(
-		"$script",
-		function(){
-			function n(n,r){
-				for(var e=0,t=n.length;t>e;++e){
-					if(!r(n[e])) return c;
-				}
-				return 1;
-			}
-			function r(r,e){
-				n(r,function(n){return!e(n)})
-			}
-			function e(i,u,c){
-				function f(n){
-					return n.call?n():d[n]
-				}
-				function s(){
-					if(!--$){
-						d[h]=1,v&&v();
-						for(var e in m){
-							n(e.split("|"),f)&&!r(m[e],f)&&(m[e]=[])
-						}
-					}
-				}
-				i=i[a]?i:[i];
-				var g=u && u.call,
-				    v=g ? u : c,
-				    h=g?i.join(""):u,
-				    $=i.length;
-				setTimeout(function(){
-					r(i,function n(r,e){
-						if(r === null){
-							return s();
-						}
-						r = e || /^https?:\/\//.test(r) || !o ? r : (o+r+".js");
-						if(p[r]){
-							h && l[h] = 1;
-							if(p[r] == 2){
-								return s();
-							} else {
-								setTimeout(n.bind(null,r,!0),0);
-							}
-							return void 0;
-						} else {
-							p[r] = 1;
-							h && (l[h] = 1);
-							t(r,s);
-							return void 0;
-						}
-					})
-				},0);
-				return e;
-			}
-			function t(n,r){
-				var e,
-				    t=i.createElement("script");
-				t.onload=t.onerror=t[s]=function(){
-					t[f] && !/^c|loade/.test(t[f]) || e || (t.onload=t[s]=null,e=1,p[n]=2,r());
-				};
-				t.async=1;
-				t.src=n;
-				u.insertBefore(t,u.lastChild);
-			}
-			var o,
-			    i=document,
-			    u=i.getElementsByTagName("head")[0],
-			    c=!1,
-			    a="push",
-			    f="readyState",
-			    s="onreadystatechange",
-			    d={},
-			    l={},
-			    m={},
-			    p={};
-			e.get = t;
-			e.order=function(n,r,t){
-				!function o(i){
-					i=n.shift(),n.length?e(i,o):e(i,r,t)
-				}()
-			};
-			e.path=function(n){
-				o=n;
-			};
-			e.ready=function(t,o,i){
-				t= t[a] ? t : [t];
-				var u=[];
-				!r(t,function(n){
-					d[n]||u[a](n)
-				}) && n(t,function(n){return d[n]}) ? o() : !function(n){
-					m[n]=m[n] || [];
-					m[n][a](o);
-					i&&i(u);
-				}(t.join("|"));
-				return e;
-			};
-			e.done=function(n){
-				e([null],n)
-			};
-			return e;
-		});
+}();
 "undefined"!=typeof dojo ? dojo.provide("org.cometd") : (this.org=this.org||{},org.cometd={});
 $script(["js/controller.min.js?v"+kahoot.version],function(){angular.bootstrap(document,["app"])});
