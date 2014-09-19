51f28b57f5d7ae4822155aa7b8bb9b6ce21dac73
root <root@kvm-test.thehawken.org>
2014-09-20 00:37:54 +0200

    r00ter - now with intelligent design and bux fixes

---
+++ ssl/r00ter.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+import ssl, socket, select, sys, threading, time
+
+
+def logit(text, connid):
+	global logdir
+	with open(logdir+"/log."+str(connid)+".txt", "a") as myfile:
+		myfile.write(text)
+		t = time.time()
+		sys.stdout.write("cid "+str(connid)+" at "+str(t)+": "+text)
+		sys.stdout.flush()
+
+
+def handle_conn(nssl, client_addr, connid, remote, logdir):
+	rsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	rssl = ssl.wrap_socket(rsock)
+	rssl.connect(remote)
+	ins = [rssl, nssl]
+	outs = []
+	while ins:
+		print "Waiting for select"
+		read, write, exc = select.select(ins, outs, ins)
+		for s in read:
+			if s == rssl:
+				data = rssl.recv()
+				print "server -> client ("+str(connid)+"): "+str(len(data))
+				if len(data) == 0:
+					ins = []
+				else:
+					nssl.send(data)
+					logit(data, connid, logdir)
+			elif s == nssl:
+				data = nssl.recv()
+				print "client -> server ("+str(connid)+"): "+str(len(data))
+				if len(data) == 0:
+					ins = []
+				else:
+					rssl.send(data)
+					logit(data, connid, logdir)
+	rssl.close()
+	nssl.close()
+
+
+def run(local, remote, cert, key, logdir):
+	lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	lsock.bind(local)
+	lsock.listen(5)
+
+	connid = 0
+
+	while True:
+		print "Waiting for clients to connect to my",local
+		nsock, addr = lsock.accept()
+		try :
+			nssl = ssl.wrap_socket(nsock, keyfile=key, certfile=cert, server_side=True)
+			t = threading.Thread(target=handle_conn, args=(nssl, addr, connid, remote, logdir))
+			t.daemon = True
+			t.start()
+			connid = connid+1
+		except:
+			print "Problems caught :("
--- ssl/r00ter1.py
+++ ssl/r00ter1.py
@@ -1,67 +1,12 @@
 #!/usr/bin/python
 
-import ssl, socket, select, sys, threading, time
+import r00ter
 
-sslport = 4002
-logdir = "r00terlog"
+logdir = "r00terlog1"
 
-remote = ('www.kahoot.it', '443')
-cert = "kahoot.it.crt"
-key = "kahoot.it.key"
+local = ('0.0.0.0', 4002)
+remote = ('www.kahoot.it', 443)
+cert = "www.kahoot.it.crt"
+key = "www.kahoot.it.key"
 
-
-
-def logit(text, connid):
-	global logdir
-	with open(logdir+"/log."+str(connid)+".txt", "a") as myfile:
-		myfile.write(text)
-		t = time.time()
-		sys.stdout.write("cid "+str(connid)+" at "+str(t)+": "+text)
-		sys.stdout.flush()
-
-
-def handle_conn(nssl, client_addr, connid):
-	global remote
-	rsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	rssl = ssl.wrap_socket(rsock)
-	rssl.connect(remote)
-	ins = [rssl, nssl]
-	outs = []
-	while ins:
-		print "Waiting for select"
-		read, write, exc = select.select(ins, outs, ins)
-		for s in read:
-			if s == rssl:
-				data = rssl.recv()
-				print "server -> client ("+str(connid)+"): "+str(len(data))
-				if len(data) == 0:
-					ins = []
-				else:
-					nssl.send(data)
-					logit(data, connid)
-			elif s == nssl:
-				data = nssl.recv()
-				print "client -> server ("+str(connid)+"): "+str(len(data))
-				if len(data) == 0:
-					ins = []
-				else:
-					rssl.send(data)
-					logit(data, connid)
-	rssl.close()
-	nssl.close()
-
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-lsock.bind(('', sslport))
-lsock.listen(5)
-
-connid = 0
-
-while True:
-	print "Waiting for clients to connect to my port "+str(sslport)
-	nsock, addr = lsock.accept()
-	nssl = ssl.wrap_socket(nsock, keyfile=key, certfile=cert, server_side=True)
-	t = threading.Thread(target=handle_conn, args=(nssl,addr,connid))
-	t.daemon = True
-	t.start()
-	connid = connid+1
+r00ter.run(local,remote,cert,key,logdir)
